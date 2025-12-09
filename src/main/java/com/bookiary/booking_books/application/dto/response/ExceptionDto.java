a73f578c31e6f605360d32a989be4bda3d08d0f2
pauloalarico <paulocraft20@gmail.com>
2025-12-08 22:23:51 -0300

    feature(api) add Exception and RestControllerAdvice for api

---
+++ src/main/java/com/bookiary/booking_books/application/dto/response/ExceptionDto.java
@@ -0,0 +1,6 @@
+package com.bookiary.booking_books.application.dto.response;
+
+public record ExceptionDto(
+        String message
+) {
+}
---
+++ src/main/java/com/bookiary/booking_books/presentation/exception/ExceptionManager.java
@@ -0,0 +1,17 @@
+package com.bookiary.booking_books.presentation.exception;
+
+import com.bookiary.booking_books.application.dto.response.ExceptionDto;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionManager {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ExceptionDto> notFound(EntityNotFoundException e) {
+        return new ResponseEntity<>(new ExceptionDto(e.getMessage()), HttpStatus.NOT_FOUND);
+    }
+}
