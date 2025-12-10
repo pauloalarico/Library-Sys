dacc72fa0cf1e116b0432b15909f45f927776d67
pauloalarico <paulocraft20@gmail.com>
2025-12-10 20:19:07 -0300

    feature(api) add password encrypter and authentication manager to securitycontroller.

--- src/main/java/com/bookiary/booking_books/presentation/controller/SecurityController.java
+++ src/main/java/com/bookiary/booking_books/presentation/controller/SecurityController.java
@@ -2,6 +2,7 @@ package com.bookiary.booking_books.presentation.controller;
 
 import com.bookiary.booking_books.application.dto.request.LoginDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,8 +15,9 @@ public class SecurityController {
     private final AuthenticationManager authenticationManager;
 
     @PostMapping
-    public void login(LoginDto dto) {
+    public ResponseEntity<Void> login(LoginDto dto) {
         var token = new UsernamePasswordAuthenticationToken(dto.username(), dto.password());
         var authentication = authenticationManager.authenticate(token);
+        return ResponseEntity.ok().build();
     }
 }
